489a1aa206a2ee9f48b54f607d4820646b559126
cryptonerdion <93843434+cryptonerdion@users.noreply.github.com>
2021-12-20 23:48:21 +0100

    Update Root.jsx

--- src/Root.jsx
+++ src/Root.jsx
@@ -15,6 +15,7 @@ export default class Root extends Component {
   render() {
     return (
       <Web3ContextProvider>
+        <Provider store={store}>
           <BrowserRouter basename={"/#"}>
             <App />
           </BrowserRouter>
