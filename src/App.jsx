2435539bfe2f0c5334ae06769804b8f2c228a575
ditinex <asifakramsk@gmail.com>
2021-12-25 16:32:09 +0530

    router fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@material-ui/core/styles";
 import { useEffect, useState, useCallback, useMemo } from "react";
-import { HashRouter, BrowserRouter as Router, Route, Redirect, Switch, useLocation, useHistory } from "react-router-dom";
+import { HashRouter, BrowserRouter, Route, Redirect, Switch, useLocation, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { Hidden, useMediaQuery } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
